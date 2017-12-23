b65ccab0645db3babbeb60435bbcda0331aeaf99
aasumitro <aasumitro@merahputih.id>
2017-12-23 16:15:24 +0800

    Initial commit

---
+++ app/src/main/java/id/asmith/someappclean/ui/main/SomeView.kt
@@ -0,0 +1,8 @@
+package id.asmith.someappclean.ui.main
+
+
+/**
+ * Created by Agus Adhi Sumitro on 23/12/2017.
+ * https://asmith.my.id
+ * aasumitro@gmail.com
+ */
---
+++ app/src/main/java/id/asmith/someappclean/ui/main/fragment/fragmentOne.kt
@@ -0,0 +1,8 @@
+package id.asmith.someappclean.ui.main.fragment
+
+
+/**
+ * Created by Agus Adhi Sumitro on 23/12/2017.
+ * https://asmith.my.id
+ * aasumitro@gmail.com
+ */
